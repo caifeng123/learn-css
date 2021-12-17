9826643c9512f90af6bdc8bcb06089932ff4471b
caifeng123 <47168663+caifeng123@users.noreply.github.com>
2021-12-17 10:04:09 +0800

    feat: transform

--- src/teach/transform/index.tsx
+++ src/teach/transform/index.tsx
@@ -3,8 +3,15 @@ import "./index.less";
 export default () => {
   return (
     <div className="App">
+      复杂图形优先考虑clip-path，文字换行问题使用border-image+svg
       <div id="Rhomboid">
         平行四边形 直接skew会导致字体也变斜 使用伪元素，注意其层级高
+      </div>
+      <div id="clip-path-rhombus">菱形</div>
+      <div id="clip-path-eight-edge">8边形</div>
+      <div id="threeD-trapezoid">3d实现梯形,修改通过transform-origin</div>
+      <div id="conic-gradient" data-value="30%">
+        饼图 角向量
       </div>
     </div>
   );
